f9008772405cd895b3b12f00f97105ff82e36e9e
gongora84 <104776654+RASCloud1@users.noreply.github.com>
2023-02-21 14:46:45 -0500

    update from vs

--- CMSSERVICE.PRESENTATION/AssemblyReference.cs
+++ CMSSERVICE.PRESENTATION/AssemblyReference.cs
@@ -2,6 +2,9 @@
 
 namespace CMSSERVICE.PRESENTATION;
 
+/// <summary>
+/// Assembly Reference.
+/// </summary>
 public static class AssemblyReference
 {
     public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
