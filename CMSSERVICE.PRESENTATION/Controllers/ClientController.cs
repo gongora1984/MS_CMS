921c705c35d54a89d0c232721103d0f4991dcc9e
gongora84 <104776654+RASCloud1@users.noreply.github.com>
2023-02-27 18:07:04 -0500

    adding client controller

---
+++ CMSSERVICE.PRESENTATION/Controllers/ClientController.cs
@@ -0,0 +1,31 @@
+﻿using CMSSERVICE.DOMAIN.Contracts.Responses.Clients;
+using CMSSERVICE.PRESENTATION.Abstractions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CMSSERVICE.PRESENTATION.Controllers;
+
+[Authorize(AuthenticationSchemes = $"{JwtBearerDefaults.AuthenticationScheme},ApiKey")]
+[Route("api/Client")]
+public sealed class ClientController : ApiController
+{
+    public ClientController(ISender sender)
+        : base(sender)
+    {
+    }
+
+    [AllowAnonymous]
+    [HttpGet("AllClients", Name = "All Clients")]
+    [ProducesResponseType(typeof(AllClientResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetAllClients(CancellationToken cancellationToken)
+    {
+        ////var command = new GetAllCompaniesQuery();
+
+        ////Result<AllCompaniesResponse> result = await Sender.Send(command, cancellationToken);
+
+        ////return Ok(result.Value);
+        await Task.FromResult(new AllClientResponse());
+        return Ok("all clients");
+    }
+}
