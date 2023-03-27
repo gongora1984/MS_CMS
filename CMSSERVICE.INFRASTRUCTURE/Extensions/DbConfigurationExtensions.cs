d8587c3332077c0f46c088e3ea18f67ecb66d05c
gongora84 <104776654+RASCloud1@users.noreply.github.com>
2023-03-27 18:59:22 -0400

    docker compose config

--- CMSSERVICE.INFRASTRUCTURE/Extensions/DbConfigurationExtensions.cs
+++ CMSSERVICE.INFRASTRUCTURE/Extensions/DbConfigurationExtensions.cs
@@ -8,18 +8,28 @@ public static class DbConfigurationExtensions
 {
     public static string BuildDbConnectionString(DatabaseOptions dbOptions)
     {
-        var builder = new SqlConnectionStringBuilder
-        {
-            DataSource = string.Format("{0},{1}", dbOptions.DbHost, dbOptions.DbPort),
-            InitialCatalog = dbOptions.DbName,
-            UserID = dbOptions.DbUser,
-            Password = dbOptions.DbPassword,
-            ConnectTimeout = dbOptions.DbCommandTimeOut,
-            TrustServerCertificate = dbOptions.DbTrustServerCertificate,
-            MultiSubnetFailover = dbOptions.DbMultiSubnetFailover,
-            MultipleActiveResultSets = dbOptions.DbMultipleActiveResultSets
-        };
-        return builder.ConnectionString;
+        ////var builder = new SqlConnectionStringBuilder
+        ////{
+        ////    DataSource = string.Format("{0},{1}", dbOptions.DbHost, dbOptions.DbPort),
+        ////    InitialCatalog = dbOptions.DbName,
+        ////    UserID = dbOptions.DbUser,
+        ////    Password = dbOptions.DbPassword,
+        ////    ConnectTimeout = dbOptions.DbCommandTimeOut,
+        ////    TrustServerCertificate = dbOptions.DbTrustServerCertificate,
+        ////    MultiSubnetFailover = dbOptions.DbMultiSubnetFailover,
+        ////    MultipleActiveResultSets = dbOptions.DbMultipleActiveResultSets
+        ////};
+        ////return builder.ConnectionString;
+        var connectionstring = $"" +
+            $"Server={dbOptions.DbHost};" +
+            $"Initial Catalog={dbOptions.DbName};" +
+            $"User ID={dbOptions.DbUser};" +
+            $"Password={dbOptions.DbPassword};" +
+            $"MultipleActiveResultSets={dbOptions.DbTrustServerCertificate};" +
+            $"TrustServerCertificate={dbOptions.DbTrustServerCertificate};" +
+            $"MultiSubnetFailover={dbOptions.DbMultiSubnetFailover}";
+
+        return connectionstring;
     }
 
     public static string BuildDbConnectionString(this IConfiguration configuration)
--- Readme.txt
+++ Readme.txt
@@ -1,5 +1,21 @@
 Docker Compose Video:
 https://www.youtube.com/watch?v=hpLvXNASyTI
 
+Docker Compose Commands:
+ - Start
+	docker-compose up
+ Stop
+	docker-compose down
+
 Docker SQL Server Command
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=0r4cl3HGC" -e "MSSQL_PID=Express" -p 8002:1433 -d mcr.microsoft.com/mssql/server:2022-latest
+
+
+Docker Command:
+ - Build Image:
+	docker build -t <<app_tag>> .
+	ex: docker build -t mailingsystem .
+ 
+ - Run Image into Container
+	docker run -p externalport:internalport <<app_tag>>
+	ex: docker run -p 8080:80 mailingsystem
